c107a0b495ef08f0ab542b3cb159604520f127bb
Mr Footwork <pandau.ting@gmail.com>
2020-10-13 23:12:19 +0200

    Finished Course Chapter Getting Started

--- 010 L Getting Started/gs-02-first-app-with-just-js/appRetry.js
+++ 010 L Getting Started/gs-02-first-app-with-just-js/appRetry.js
@@ -1,6 +1,13 @@
-const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
 const listEl = document.querySelector('ul');
+
+function inputEnter(enterPressed) {
+  console.log(enterPressed);
+  if (enterPressed.key == 'Enter') {
+    buttonEl.click();
+  }
+}
 
 function addGoal() {
   const enteredValue = inputEl.value;
@@ -10,4 +17,5 @@ function addGoal() {
   inputEl.value = '';
 }
 
+inputEl.addEventListener('keypress', inputEnter);
 buttonEl.addEventListener('click', addGoal);
