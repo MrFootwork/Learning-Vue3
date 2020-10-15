6f8538dd0e25ee3788ef572a991ed158fc716da3
Mr Footwork <pandau.ting@gmail.com>
2020-10-15 22:44:28 +0200

    Event Modifiers finished!

--- 020 L DOM Interaction/basics-03-events-starting-code/app.js
+++ 020 L DOM Interaction/basics-03-events-starting-code/app.js
@@ -2,14 +2,29 @@ const app = Vue.createApp({
 	data() {
 		return {
 			counter: 0,
+			howMuch: 0,
+			fullName: '',
+			confirmedName: '',
 		}
 	},
 	methods: {
-		addToCounter() {
-			this.counter++
+		addToCounter(number) {
+			this.counter += number
 		},
-		removeFromCounter() {
-			this.counter--
+		removeFromCounter(number) {
+			this.counter -= number
+		},
+		storeNumber(event) {
+			this.howMuch = +event.target.value
+		},
+		storeName(event, lastName) {
+			this.fullName = `${event.target.value} ${lastName}`
+		},
+		confirmName(event) {
+			this.confirmedName = this.fullName
+		},
+		submitForm() {
+			alert('Submitted!')
 		},
 	},
 })
