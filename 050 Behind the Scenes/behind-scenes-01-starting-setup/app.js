fa0e06b823275a73e490a3fa70a73841cd05bceb
Mr Footwork <pandau.ting@gmail.com>
2020-11-01 08:08:39 +0100

    Under the Hood finished
    Next: Components

--- 050 Behind the Scenes/behind-scenes-01-starting-setup/app.js
+++ 050 Behind the Scenes/behind-scenes-01-starting-setup/app.js
@@ -1,18 +1,38 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
-    setText() {
-      this.message = this.currentUserInput;
-    },
-  },
-});
+const app = {
+	data() {
+		return {
+			currentUserInput: '',
+			message: 'Vue is great!',
+		}
+	},
+	methods: {
+		saveInput(event) {
+			this.currentUserInput = event.target.value
+		},
+		setText() {
+			// this.message = this.currentUserInput
+			this.message = this.$refs.eingabe.value
+			console.dir(this.$refs.eingabe)
+		},
+	},
+	beforeCreate() {
+		console.log('beforeCreate()')
+	},
+	created() {
+		console.log('created()')
+	},
+	beforeMount() {
+		console.log('beforeMount()')
+	},
+	mounted() {
+		console.log('mounted()')
+	},
+	beforeUpdate() {
+		console.log('beforeUpdate()')
+	},
+	updated() {
+		console.log('updated()')
+	},
+}
 
-app.mount('#app');
+Vue.createApp(app).mount('#app')
