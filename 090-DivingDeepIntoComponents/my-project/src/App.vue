2a833c6d2a1c9d904e01f010545a5a98b063064c
Mr Footwork <pandau.ting@gmail.com>
2020-11-11 23:44:15 +0100

    Finished Global and Local Components

--- 090-DivingDeepIntoComponents/my-project/src/App.vue
+++ 090-DivingDeepIntoComponents/my-project/src/App.vue
@@ -1,35 +1,45 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
+	<div>
+		<TheHeader />
+		<BadgeList />
+		<UserInfo
+			:full-name="activeUser.name"
+			:info-text="activeUser.description"
+			:role="activeUser.role"
+		/>
+	</div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
-};
+	import TheHeader from './components/TheHeader.vue'
+	import BadgeList from './components/BadgeList.vue'
+	import UserInfo from './components/UserInfo.vue'
+
+	export default {
+		components: {
+			// 'the-header': TheHeader,
+			TheHeader,
+			BadgeList,
+			UserInfo,
+		},
+		data() {
+			return {
+				activeUser: {
+					name: 'Maximilian Schwarzmüller',
+					description: 'Site owner and admin',
+					role: 'admin',
+				},
+			}
+		},
+	}
 </script>
 
 <style>
-html {
-  font-family: sans-serif;
-}
+	html {
+		font-family: sans-serif;
+	}
 
-body {
-  margin: 0;
-}
+	body {
+		margin: 0;
+	}
 </style>
