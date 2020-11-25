219ab3c15b78bf47ba82541538b06dcbaa92e76f
Mr Footwork <pandau.ting@gmail.com>
2020-11-25 23:32:11 +0100

    Started with Dynamic Components
    Next: Data Flow needs adjustments

--- 100-ProjectLearningResources/prj-cmp-01-starting-setup/src/App.vue
+++ 100-ProjectLearningResources/prj-cmp-01-starting-setup/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <TheHeader title="RememberMe" />
-  <StoredResources :resources="storedResources" />
+  <TheResources />
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
-    StoredResources,
-    TheHeader
+    TheHeader,
+    TheResources
   },
   data() {
     return {
---
+++ 100-ProjectLearningResources/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>TEST: Add Resource</h2>
+</template>
