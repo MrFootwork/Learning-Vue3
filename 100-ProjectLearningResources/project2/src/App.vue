2eb57c70d410b35ba3354858f52acf68e08e7221
Mr Footwork <pandau.ting@gmail.com>
2020-12-02 00:35:28 +0100

    Finished Setup and First steps
    Next: Continue with First Components & Props

---
+++ 100-ProjectLearningResources/project2/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: { LearningResource },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-giude',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
